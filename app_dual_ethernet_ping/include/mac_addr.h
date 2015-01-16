3a45610c640fbf55328edfb2e44485d8be1aad08
Agustin <tenagarcia.agustin@gmail.com>
2015-01-16 17:57:41 +0100

    Put MAC management in separate files

---
+++ app_dual_ethernet_ping/include/mac_addr.h
@@ -0,0 +1,12 @@
+#ifndef __mac_addr_h__
+#define __mac_addr_h__
+
+#include <print.h>
+
+extern const unsigned char MAC_ADDRESS_P1[6];
+extern const unsigned char MAC_ADDRESS_P2[6];
+
+void init_macAddress_p1(char mac_address[6]);
+void init_macAddress_p2(char mac_address[6]);
+
+#endif
