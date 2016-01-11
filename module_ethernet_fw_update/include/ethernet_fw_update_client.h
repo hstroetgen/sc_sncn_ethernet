7fe7699f89d2cbdc2589b97fd6a4ea738da33002
Henrik Stroetgen <hstroetgen@synapticon.com>
2016-01-11 11:50:02 +0100

    add new header files

---
+++ module_ethernet_fw_update/include/ethernet_fw_update_client.h
@@ -0,0 +1,14 @@
+/*
+ * flash_over_ethernet.h
+ *
+ *  Created on: 27.10.2015
+ *      Author: hstroetgen
+ */
+
+#ifndef FLASH_OVER_ETHERNET_CLIENT_H_
+#define FLASH_OVER_ETHERNET_CLIENT_H_
+
+
+int fwUpdt_filter(char data[], int &nbytes, chanend c_flash_data);
+
+#endif /* FLASH_OVER_ETHERNET_CLIENT_H_ */
---
+++ module_ethernet_fw_update/include/ethernet_fw_update_server.h
@@ -0,0 +1,15 @@
+/*
+ * ethernet_fw_update_server.h
+ *
+ *  Created on: 11.01.2016
+ *      Author: hstroetgen
+ */
+
+#ifndef ETHERNET_FW_UPDATE_SERVER_H_
+#define ETHERNET_FW_UPDATE_SERVER_H_
+
+#include <flash.h> // FIXME This dependencies should be removed
+
+void fwUpdt_server(fl_SPIPorts &SPI, chanend c_flash_data);
+
+#endif /* ETHERNET_FW_UPDATE_SERVER_H_ */
